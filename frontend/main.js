19898f172e17815c65438f54083a04baf71d65dd
leojemba <jembaleo@yahoo.com>
2022-01-05 05:46:47 +0100

    Update to structure, created main.js file

---
+++ frontend/main.js
@@ -0,0 +1,21 @@
+windows.addEventlistener('DOMContentLoaded', (event) => {
+    getVisitCount();
+})
+
+
+const function = '';
+
+const getVisitCount = () => {
+    let count = 30
+    fetch(funtionApi).then(response => {
+        return response.json()
+    }).then(response log(...data: any[]): void
+        console.log("Website called funtion API.");
+    count = response.count;
+    document.getElementById("counter").innerText = count;
+}).catch (funtion)(error);
+console.log(error);
+});
+return count;
+
+}
